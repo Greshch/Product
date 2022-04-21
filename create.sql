535866e8b3a519965b6e0291186baa77a9a49ed1
Greshch <greshchook@gmail.com>
2022-04-21 19:46:39 +0300

    Country::ADD Czech

--- create.sql
+++ create.sql
@@ -129,3 +129,4 @@ INSERT INTO Country(name) VALUES( 'Italy' );
 INSERT INTO Country(name) VALUES( 'Netherland' );
 INSERT INTO Country(name) VALUES( 'Spain' );
 INSERT INTO Country(name) VALUES( 'Portugal' );
+INSERT INTO Country(name) VALUES( 'Czech' );
